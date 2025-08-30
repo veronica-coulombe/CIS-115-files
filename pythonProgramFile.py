808974966973295bb96ec90a68feb3e04e1d3560
Veronica Coulombe <156690411+veronica-coulombe@users.noreply.github.com>
2025-08-30 09:33:32 -0400

    Add files via upload

---
+++ pythonProgramFile.py
@@ -0,0 +1,12 @@
+print("Hello Everyone!")
+
+studentName = input("What is your name? ")
+print(studentName, ", it's nice to meet you!")
+
+studentMajor = input("What is your major? ")
+print(studentMajor, "is a very cool major", studentName)
+
+studentHobby = input("So, what is your favorite thing to do? ")
+print(studentHobby, "is a fun hobby!")
+
+print("Well, it was nice to meet you", studentName)
